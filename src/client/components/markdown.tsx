73798df8f72aa9db1754a1d5bcc12690dc54137b
Eric Wooley <ericwooley@gmail.com>
2020-03-09 13:10:26 -0700

    white behind markdown

--- src/client/components/markdown.tsx
+++ src/client/components/markdown.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
+import styled from 'styled-components'
+const Wrapper = styled.div`
+  color: black;
+  background: rgba(255, 255, 255, 0.75);
+  border-radius: 3px;
+  margin: 3px;
+  padding: 5px;
+`
 
 export default class Markdown extends React.PureComponent<{
   children: string
 }> {
   render() {
-    return <ReactMarkdown source={this.props.children} />
+    return (
+      <Wrapper>
+        <ReactMarkdown source={this.props.children} />
+      </Wrapper>
+    )
   }
 }
